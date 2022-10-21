9a58cce188beb720b9761d0e28aa05de25543dcb
Carlton Nunes Desouza <cj@Carltons-MacBook-Air.local>
2022-10-22 00:05:18 +0100

    Major structure refactor

--- src/app.py
+++ src/app.py
@@ -1,68 +1,142 @@
 #!/usr/bin/env python3
 import os
 
-from db.db import create, delete
-from db.db import product_list as data
-from db.db import read, update
+from db.db import create, delete, read, update
 
-MAIN_MENU = """1) Start Application
-0) Exit Application
->>> """
-PRODUCT_MENU = """1) Get Products List
-2) Add New Product
-3) Update Existing Product (WIP)
-4) Delete Product (WIP)
-0) Main Menu
->>> """
 
-os.system("clear")
-main_menu_input = input(MAIN_MENU)
-while main_menu_input != "0":
-    if main_menu_input == "1":
-        os.system("clear")
-        product_menu_input = input(PRODUCT_MENU)
-        while product_menu_input != "0":
-            if product_menu_input == "1":
+def print_menu(menu):
+    prompt = ">>> "
+    for option, message in menu.items():
+        print(f"{option}) {message}")
+
+    return input(prompt)
+
+
+def product_menu_loop(data, product_menu, bool_menu):
+    user_input = print_menu(product_menu)
+    while user_input != "0":
+        # Print list of products
+        if user_input == "1":
+            os.system("clear")
+            read(data)
+            user_input = print_menu(product_menu)
+
+        # Add new product to db
+        elif user_input == "2":
+            os.system("clear")
+            new_product = input("Enter Product Name:\n>>> ").strip()
+            if not new_product:
                 os.system("clear")
-                read(data)
-                product_menu_input = input(f"\n{PRODUCT_MENU}")
+                print("Operation Canceled!\n")
+                user_input = print_menu(product_menu)
+                continue
 
-            elif product_menu_input == "2":
-                os.system("clear")
-                create(data)
-                product_menu_input = input(PRODUCT_MENU)
+            create(data, new_product)
+            os.system("clear")
+            print(f"New Product: {new_product}!\n")
+            user_input = print_menu(product_menu)
 
-            elif product_menu_input == "3":
+        # Update existing product
+        elif user_input == "3":
+            os.system("clear")
+            product_found = read(data)
+            if not product_found:
                 os.system("clear")
-                product_found = read(data)
-                if not product_found:
-                    product_menu_input = input(f"\n{PRODUCT_MENU}")
+                print("Operation Canceled!\n")
+                user_input = print_menu(product_menu)
+                continue
+
+            try:
+                product_index = int(input("Select Product To Update:\n>>> ")) - 1
+                new_product_name = input(f"\nEnter New Product Name:\n>>> ").strip()
+                print(f"\nUpdate '{data[product_index]}' -> '{new_product_name}'?")
+                confirmation = print_menu(bool_menu)
+                if confirmation == "0":
+                    os.system("clear")
+                    print("Operation Canceled!\n")
                     continue
 
-                update(data)
-                product_menu_input = input(PRODUCT_MENU)
+                if confirmation == "1":
+                    os.system("clear")
+                    print("Product Name Updated!\n")
+                    update(data, product_index, new_product_name)
 
-            elif product_menu_input == "4":
+            except ValueError:
                 os.system("clear")
-                product_found = read(data)
-                if not product_found:
-                    product_menu_input = input(f"\n{PRODUCT_MENU}")
+                print("Invalid Input!\n")
+            except IndexError:
+                os.system("clear")
+                print("Invalid Input!\n")
+            finally:
+                user_input = print_menu(product_menu)
+
+        # Delete product from db
+        elif user_input == "4":
+            os.system("clear")
+            product_found = read(data)
+            if not product_found:
+                user_input = print_menu(product_menu)
+                continue
+
+            try:
+                product_index = int(input("\nSelect Product To Delete:\n>>> ")) - 1
+                print(f"Delete '{data[product_index]}'?\n")
+                confirmation = print_menu(bool_menu)
+                if confirmation == "0":
+                    os.system("clear")
+                    print("Operation Canceled!\n")
                     continue
 
-                delete(data)
-                product_menu_input = input(PRODUCT_MENU)
+                if confirmation == "1":
+                    os.system("clear")
+                    print("Product Deleted!\n")
+                    delete(data, product_index)
 
-            else:
+            except ValueError:
                 os.system("clear")
-                print(f"'{product_menu_input}' Is Not A Valid Command!")
-                product_menu_input = input(f"\n{PRODUCT_MENU}")
+                print("Invalid Input!\n")
+            except IndexError:
+                os.system("clear")
+                print("Invalid Input!\n")
+            finally:
+                user_input = print_menu(product_menu)
 
-        os.system("clear")
+        else:
+            os.system("clear")
+            print(f"'{user_input}' Is Not A Valid Command!\n")
+            user_input = print_menu(product_menu)
 
-    else:
-        os.system("clear")
-        print(f"'{main_menu_input}' Is Not A Valid Command!\n")
+    os.system("clear")
 
-    main_menu_input = input(MAIN_MENU)
 
-print("\nExiting Application...")
+def main_menu_loop(data, main_menu, product_menu, bool_menu):
+    os.system("clear")
+    user_input = print_menu(main_menu)
+    while user_input != "0":
+        if user_input == "1":
+            os.system("clear")
+            product_menu_loop(data, product_menu, bool_menu)
+
+        else:
+            os.system("clear")
+            print(f"'{user_input}' Is Not A Valid Command!\n")
+
+        user_input = print_menu(main_menu)
+
+
+if __name__ == "__main__":
+    MAIN_MENU = {1: "Start Application", 0: "Exit Application"}
+    PRODUCT_MENU = {
+        1: "Get Products List",
+        2: "Add New Product",
+        3: "Update Existing Product",
+        4: "Delete Product",
+        0: "Main Menu",
+    }
+    BOOL_MENU = {1: "Yes", 0: "No"}
+
+    # Non persistant data
+    data = []
+
+    main_menu_loop(data, MAIN_MENU, PRODUCT_MENU, BOOL_MENU)
+    print("\nExiting Application...")
--- src/db/db.py
+++ src/db/db.py
@@ -1,80 +1,23 @@
-import os
-
-PROMPT = """1) Yes
-0) No
->>> """
-
-product_list = []
-
-
-def create(data):
-    new_product = input("Enter Product Name:\n>>> ").strip()
-    data.append(new_product)
-    os.system("clear")
-    return print(f"New Product #{len(data)}: {new_product}!\n")
+def create(data, new_product):
+    return data.append(new_product)
 
 
 def read(data):
     if not data:
-        print("No Products Currently Available!")
+        print("No Products Currently Available!\n")
         return False
 
     print("Products:")
     for index, product in enumerate(data, 1):
         print(f"{index}) {product.title()}")
 
+    print()
     return True
 
 
-def update(data):
-    try:
-        product_index = int(input("\nSelect Product To Update:\n>>> ")) - 1
-    except ValueError:
-        return print("Invalid Input!\n")
-
-    if product_index > len(data) or product_index < 0:
-        os.system("clear")
-        return print("Invalid Input!\n")
-
-    os.system("clear")
-    new_product_name = input(f"Enter New Product Name:\n>>> ").strip()
-    os.system("clear")
-    print(f"Update '{data[product_index]}' -> '{new_product_name}'?\n")
-    confirmation = input(PROMPT).lower()
-
-    if confirmation == "0":
-        return os.system("clear")
-
-    if confirmation == "1":
-        data[product_index] = new_product_name
-        os.system("clear")
-        return print("Product Name Updated!\n")
-
-    os.system("clear")
-    return print(f"'{confirmation}' Is Not A Valid Command!\n")
+def update(data, old_product, new_product):
+    data[old_product] = new_product
 
 
-def delete(data):
-    try:
-        product_index = int(input("\nSelect Product To Delete:\n>>> ")) - 1
-    except ValueError:
-        return print("Invalid Input!\n")
-
-    if product_index > len(data) or product_index < 0:
-        os.system("clear")
-        return print("Invalid Input!\n")
-
-    os.system("clear")
-    print(f"Delete '{data[product_index]}'?\n")
-    confirmation = input(PROMPT).lower()
-
-    if confirmation == "0":
-        return os.system("clear")
-
-    if confirmation == "1":
-        data.pop(product_index)
-        os.system("clear")
-        return print("Product Deleted!")
-
-    os.system("clear")
-    return print(f"'{confirmation}' Is Not A Valid Command!\n")
+def delete(data, product_index):
+    data.pop(product_index)
