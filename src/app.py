2285175993b53161616158419408131952c77d96
Carlton (CJ) <carltonlnd@hotmail.com>
2022-10-31 20:02:23 +0000

    Added get method for printing data to screen, and update + delete for products/courier

--- src/app.py
+++ src/app.py
@@ -14,8 +14,9 @@ console = Console(
 
 
 class Menu:
-    def __init__(self, options) -> None:
+    def __init__(self, options, data_type="") -> None:
         self.options: dict = options
+        self.data_type = data_type
 
     def loop(self):
         while True:
@@ -27,7 +28,7 @@ class Menu:
 
             if cmd not in self.options.keys():
                 os.system("clear")
-                console.print("Invalid Option!\n", style="warn")
+                console.print("Invalid Option!\n", style="error")
                 continue
 
             os.system("clear")
@@ -42,6 +43,22 @@ class Menu:
 
         if "0" not in self.options.keys():
             console.print(f"[notify]0)[/notify] [base]Return[/base]")
+
+    def get_data(self) -> bool:
+        data = db.read(self.data_type)
+        if not data:
+            console.print("No Data!\n", style="notify")
+            return False
+
+        for id, entry in enumerate(data, 1):
+            # TODO: print correct data from order dictionary
+            if isinstance(entry, dict):
+                print("THIS IS A DICT")
+            else:
+                console.print(f"[notify]{id})[/notify] [base]{entry.name}[/base]")
+        else:
+            print()
+        return True
 
     @staticmethod
     def prompt():
@@ -81,17 +98,15 @@ class MainMenu(Menu):
 
 class OrderMenu(Menu):
     def __init__(self) -> None:
+        self.data_type = "orders"
         self.options = {
-            "1": self.get_orders,
+            "1": self.get_data,
             "2": self.create_order,
             "3": self.update_status,
             "4": self.update_order,
             "5": self.delete_order,
         }
-        super().__init__(self.options)
-
-    def get_orders(self):
-        pass
+        super().__init__(self.options, self.data_type)
 
     def create_order(self):
         pass
@@ -108,57 +123,83 @@ class OrderMenu(Menu):
 
 class ProductMenu(Menu):
     def __init__(self) -> None:
+        self.data_type = "products"
         self.options = {
-            "1": self.get_products,
+            "1": self.get_data,
             "2": self.add_product,
             "3": self.update_product,
             "4": self.delete_product,
         }
-        super().__init__(self.options)
+        super().__init__(self.options, self.data_type)
 
-    def get_products(self) -> None:
-        products = db.read("products")
-        if not products:
-            return console.print("No Products!\n", style="notify")
-
-        for id, entry in enumerate(products, 1):
-            console.print(f"[notify]{id})[/notify] [base]{entry.name}[/base]")
-        else:
-            print()
-
-    def add_product(self):
+    def add_product(self) -> None:
         console.print("Enter New Product Name:", style="base")
         new_product = self.prompt().title()
         db.create_product(name=new_product)
         os.system("clear")
         console.print(f"Product '{new_product}' Added!\n", style="notify")
 
-    def update_product(self):
-        pass
+    def update_product(self) -> None:
+        while True:
+            if not self.get_data():
+                return
+
+            console.print("Enter Product Number To Update:", style="notify")
+            try:
+                selection = int(self.prompt()) - 1
+            except ValueError:
+                os.system("clear")
+                console.print("Invalid Option!\n", style="error")
+                continue
+
+            if selection in range(len(db.products)):
+                break
+
+            os.system("clear")
+            console.print("Invalid Option!\n", style="error")
+
+        os.system("clear")
+        console.print("Enter New Product Name:", style="notify")
+        new_name = self.prompt().title()
+        db.products[selection].name = new_name
+        os.system("clear")
+        console.print("Updated Product!\n", style="notify")
 
     def delete_product(self):
-        pass
+        while True:
+            if not self.get_data():
+                return
+
+            console.print("Enter Product Number To Delete:", style="notify")
+            try:
+                selection = int(self.prompt()) - 1
+            except ValueError:
+                os.system("clear")
+                console.print("Invalid Option!\n", style="error")
+                continue
+
+            if selection in range(len(db.products)):
+                break
+
+            os.system("clear")
+            console.print("Invalid Option!\n", style="error")
+
+        os.system("clear")
+        db.products.pop(selection)
+        os.system("clear")
+        console.print("Deleted Product!\n", style="notify")
 
 
 class CourierMenu(Menu):
     def __init__(self) -> None:
+        self.data_type = "couriers"
         self.options = {
-            "1": self.get_couriers,
+            "1": self.get_data,
             "2": self.add_courier,
             "3": self.update_courier,
             "4": self.delete_courier,
         }
-        super().__init__(self.options)
-
-    def get_couriers(self) -> None:
-        couriers = db.read("couriers")
-        if not couriers:
-            return console.print("No Couriers!\n", style="notify")
-
-        for id, entry in enumerate(couriers, 1):
-            console.print(f"[notify]{id})[/notify] [base]{entry.name}[/base]")
-        else:
-            print()
+        super().__init__(self.options, self.data_type)
 
     def add_courier(self):
         console.print("Enter New Courier Name:", style="base")
@@ -167,11 +208,55 @@ class CourierMenu(Menu):
         os.system("clear")
         console.print(f"Courier '{new_courier}' Added!\n", style="notify")
 
-    def update_courier(self):
-        pass
+    def update_courier(self) -> None:
+        while True:
+            if not self.get_data():
+                return
+
+            console.print("Enter Courier Number To Update:", style="notify")
+            try:
+                selection = int(self.prompt()) - 1
+            except ValueError:
+                os.system("clear")
+                console.print("Invalid Option!\n", style="error")
+                continue
+
+            if selection in range(len(db.couriers)):
+                break
+
+            os.system("clear")
+            console.print("Invalid Option!\n", style="error")
+
+        os.system("clear")
+        console.print("Enter New Courier Name:", style="notify")
+        new_name = self.prompt().title()
+        db.couriers[selection].name = new_name
+        os.system("clear")
+        console.print("Updated Courier!\n", style="notify")
 
     def delete_courier(self):
-        pass
+        while True:
+            if not self.get_data():
+                return
+
+            console.print("Enter Courier Number To Delete:", style="notify")
+            try:
+                selection = int(self.prompt()) - 1
+            except ValueError:
+                os.system("clear")
+                console.print("Invalid Option!\n", style="error")
+                continue
+
+            if selection in range(len(db.couriers)):
+                break
+
+            os.system("clear")
+            console.print("Invalid Option!\n", style="error")
+
+        os.system("clear")
+        db.couriers.pop(selection)
+        os.system("clear")
+        console.print("Deleted Product!\n", style="notify")
 
 
 if __name__ == "__main__":
