07f4f9427ae2e06843eef4654dfb6576c372ef0a
Carlton Nunes Desouza <cj@Carltons-MacBook-Air.local>
2022-11-20 11:51:51 +0000

    Removed unneeded Protocol

--- src/domain/repository.py
+++ src/domain/repository.py
@@ -1,11 +1,5 @@
-from typing import Protocol
-
 from sqlalchemy import Table
 from sqlalchemy.orm import Session
-
-
-class Row(Protocol):
-    ...
 
 
 class Repository:
@@ -14,15 +8,19 @@ class Repository:
     def __init__(self, session: Session) -> None:
         self.session = session
 
-    def add(self, model: Row) -> None:
+    def add(self, model) -> None:
         """Adds model to database session"""
         self.session.add(model)
 
-    def get(self, table: Table, filter) -> Row:
-        """Returns row in table based on filter reference"""
-        return self.session.query(table).filter_by(reference=filter).one()
+    def delete(self, table: Table, id: int):
+        """Deletes row in table by unique ID"""
+        self.session.query(table).filter_by(id=id).delete()
 
-    def list(self, table: Table) -> list[Row]:
+    def get(self, table: Table, id: int):
+        """Returns row in table based on filter reference"""
+        return self.session.query(table).filter_by(id=id).one()
+
+    def list(self, table: Table) -> list:
         """Returns list of all rows in table, or empty list"""
         return self.session.query(table).all()
 
