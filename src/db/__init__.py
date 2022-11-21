5167c5962c47e6765b8117efb9a1d8ca81496cd3
Carlton (CJ) <carltonlnd@hotmail.com>
2022-11-21 21:30:57 +0000

    Moved table mapping to setup function

--- src/db/__init__.py
+++ src/db/__init__.py
@@ -5,13 +5,9 @@ from .couriers import map_couriers
 from .orders import map_orders
 from .products import map_products
 
-mapper_registry = registry()
-
-map_products(mapper_registry)
-map_couriers(mapper_registry)
-map_orders(mapper_registry)
-
 DATA_PATH = "data/cafe.db"
+
+mapper_registry = registry()
 
 
 def setup_lite_db():
@@ -21,5 +17,8 @@ def setup_lite_db():
 
 
 def create_lite_session():
+    map_products(mapper_registry)
+    map_couriers(mapper_registry)
+    map_orders(mapper_registry)
     db = setup_lite_db()
     return sessionmaker(db)
